19fbedb9bde3487fcfc1c365c16a46fa867d3c1d
Matous Jobanek <mjobanek@redhat.com>
2021-03-04 22:41:40 +0100

    action

--- make/docker.mk
+++ make/docker.mk
@@ -26,6 +26,21 @@ ifeq ($(QUAY_NAMESPACE),${GO_PACKAGE_ORG_NAME})
 endif
 	$(Q)docker push ${IMAGE}
 
+.PHONY: podman-image
+## Build the binary image locally that can be deployed (only contains bare operator)
+podman-image: build
+	$(Q)podman build -f build/Dockerfile -t ${IMAGE} .
+
+.PHONY: podman-push
+## Push the binary image to quay.io registry
+podman-push: podman-image
+ifeq ($(QUAY_NAMESPACE),${GO_PACKAGE_ORG_NAME})
+	@echo "#################################################### WARNING ####################################################"
+	@echo you are going to push to $(QUAY_NAMESPACE) namespace, make sure you have set QUAY_NAMESPACE variable appropriately
+	@echo "#################################################################################################################"
+endif
+	$(Q)podman push ${IMAGE}
+
 .PHONY: docker-push-to-local
 ## Push the docker image to the local docker.io registry
 docker-push-to-local: set-local-registry docker-image docker-push
